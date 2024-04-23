dbc8aa07bd4195405fa2de0f9ed1b04b7a3106c8
Jung Jingyu <jjggu@naver.com>
2024-04-23 10:51:25 +0900

    Title: 세 수의 합과 평균 구하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240423/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -0,0 +1,16 @@
+num = input().split(' ')
+
+a = num[0]
+b = num[1]
+c = num[2]
+
+a, b, c = int(a), int(b), int(c)
+
+hap = a + b + c
+avg = hap / 3
+
+hap = int(hap)
+avg = int(avg)
+
+print(hap)
+print(avg)
