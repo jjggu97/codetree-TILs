863179475d7f217d710c8896c918c848d4a3f897
Jung Jingyu <jjggu@naver.com>
2024-04-24 18:30:22 +0900

    Title: 두 사람, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240424/두 사람/two-person.py
+++ 240424/두 사람/two-person.py
@@ -4,7 +4,20 @@ b = input().split(' ')
 a_age, a_sex = int(a[0]), str(a[1])
 b_age, b_sex = int(b[0]), str(b[1])
 
-if a_age >= 19 or b_age >= 19 and a_sex == 'M' or b_sex == 'M':
+a_info = 0
+b_info = 0
+
+if a_age >= 19 and a_sex == 'M':
+    a_info = 1
+else:
+    a_info = 0
+
+if b_age >= 19 and b_sex == 'M':
+    b_info = 1
+else:
+    b_info = 0
+
+if a_info == 1 or b_info == 1:
     print("1")
 else:
     print("0")
