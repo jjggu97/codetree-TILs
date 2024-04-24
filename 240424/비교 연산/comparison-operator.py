d853e3c2e05c067636bec3151128e84e35877c17
Jung Jingyu <jjggu@naver.com>
2024-04-24 14:00:58 +0900

    Title: 비교 연산, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240424/비교 연산/comparison-operator.py
@@ -0,0 +1,28 @@
+num = input().split(' ')
+
+a, b = int(num[0]), int(num[1])
+
+if a >= b:
+    print("1")
+else:
+    print("0")
+if a > b:
+    print("1")
+else:
+    print("0")
+if b >= a:
+    print("1")
+else:
+    print("0")
+if b > a:
+    print("1")
+else:
+    print("0")
+if a == b:
+    print("1")
+else:
+    print("0")
+if a != b:
+    print("1")
+else:
+    print("0")
