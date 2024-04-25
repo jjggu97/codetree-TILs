4cf61383b7ef8e59ae1e09e7c9580dd75093b954
Jung Jingyu <jjggu@naver.com>
2024-04-25 15:31:22 +0900

    Title: 최댓값 구하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/최댓값 구하기/maximum-value.py
@@ -0,0 +1,11 @@
+num = input().split(' ')
+
+a,b,c = int(num[0]),int(num[1]),int(num[2])
+
+if a > b and a > c:
+    print(a)
+else:
+    if b > a and b > c:
+        print(b)
+    else:
+        print(c)
