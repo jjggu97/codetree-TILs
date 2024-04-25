2c1dfd3d80537684f90b4f2698dd21d3dff58680
Jung Jingyu <jjggu@naver.com>
2024-04-25 13:52:23 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240425/윤년인가/is-leap-year.py
+++ 240425/윤년인가/is-leap-year.py
@@ -1,9 +1,9 @@
 year = int(input())
 
-if year % 4 == 0:
-    print("true")
+if year % 100 == 0 and year % != 400:
+    print("false")
 else:
-    if year % 100 == 0 and year % 400 != 0:
+    if year % 4 == 0:
         print("true")
     else:
         print("false")
