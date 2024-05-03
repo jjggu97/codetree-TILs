d4ca16506da6c634d6485af86b51e0978e31f9b8
Jung Jingyu <jjggu@naver.com>
2024-05-03 16:38:24 +0900

    Title: 369 게임, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240503/369 게임/369-game.py
+++ 240503/369 게임/369-game.py
@@ -4,7 +4,7 @@ alert = [3,6,9]
 
 for i in range(1, n + 1):
 
-    if i % 3 == 0 or str(i) in alert:
+    if i % 3 == 0 or i in alert:
         print("0",end=' ')
     else:
         print(i,end=' ')
