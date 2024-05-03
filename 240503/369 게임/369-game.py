f00572537ce6d118d17884ee3d2ecee2f7af82ec
Jung Jingyu <jjggu@naver.com>
2024-05-03 16:41:11 +0900

    Title: 369 게임, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/369 게임/369-game.py
+++ 240503/369 게임/369-game.py
@@ -7,7 +7,7 @@ for i in range(1, n + 1):
     if i % 3 == 0:
         print("0",end=' ')
     
-    elif i in alert:
+    elif '3' in str(i) or '6' in str(i) or '9' in str(i):
         print("0",end=' ')
     else:
         print(i,end=' ')
