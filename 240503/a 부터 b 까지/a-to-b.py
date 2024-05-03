ba24c2d335918b73acf8388216d031b4a15e613e
Jung Jingyu <jjggu@naver.com>
2024-05-03 16:20:45 +0900

    Title: a 부터 b 까지, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/a 부터 b 까지/a-to-b.py
+++ 240503/a 부터 b 까지/a-to-b.py
@@ -1,12 +1,12 @@
 n = input().split(' ')
 
 a,b = int(n[0]),int(n[1])
-print(a,end=' ')
 
-while a < b:
+while a <= b:
+
+    print(a,end=' ')
+
     if a % 2 == 1:
         a *= 2
-        print(a,end=' ')
     else:
         a += 3
-        print(a,end=' ')
