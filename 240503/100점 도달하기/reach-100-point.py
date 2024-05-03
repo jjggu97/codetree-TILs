761695130b3bc893ec3cadde3680a1a3d6c31c62
Jung Jingyu <jjggu@naver.com>
2024-05-03 13:40:24 +0900

    Title: 100점 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240503/100점 도달하기/reach-100-point.py
+++ 240503/100점 도달하기/reach-100-point.py
@@ -3,11 +3,14 @@ n = int(input())
 for i in range(n,101):
     if n >= 90:
         print("A",end=' ')
-    if n >= 80:
-        print("B",end=' ')
-    if n >= 70:
-        print("C",end=' ')
-    if n >= 60:
-        print("D",end=' ')
     else:
-        print("F",end=' ')
+        if n >= 80:
+            print("B",end=' ')
+        else:
+            if n >= 70:
+                print("C",end=' ')
+                else:
+                    if n >= 60:
+                        print("D",end=' ')
+                    else:
+                        print("F",end=' ')
